9ec3bf6149182033f7b68bf7ba05a59e6ddcc47b
Jonyuwu <jonathanjesusabrajanhau@gmail.com>
2023-11-29 14:50:02 -0500

    feat(Sidebar/Header.ts):VariantDefaultFixed

--- src/components/Sidebar/Header.ts
+++ src/components/Sidebar/Header.ts
@@ -6,7 +6,7 @@ export const Header = cva(
     variants: {
       Vheader: {
         default:
-          'fixed w-full bg-gray-900 p-5 text-white flex relative justify-between items-center z-50',
+          'fixed w-full bg-gray-900 p-5 text-white flex  justify-between items-center z-50',
         destructive: 'bg-black-300',
       },
     },
